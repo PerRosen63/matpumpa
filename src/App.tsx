4b5d7e1d4c824b8f12a0fbbfb749814149d38d40
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-16 00:01:06 +0200

    ✨ Added pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
+import "./App.css";
 
 interface AppProps {
   baseUrl: string;
@@ -7,49 +8,11 @@ interface AppProps {
   consumerSecret: string;
 }
 
-function App({ baseUrl, consumerKey, consumerSecret }: AppProps) {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch(
-        `${baseUrl}?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`
-      );
-      const data = await response.json();
-      setProducts(data);
-      setLoading(false);
-    };
-
-    fetchProducts();
-  }, [baseUrl, consumerKey, consumerSecret]);
-
-  return loading ? (
-    <div className="loaderText">
-      <h2>Just a moment. Fetching products...</h2>{" "}
-    </div>
-  ) : (
-    <ul>
-      {products ? (
-        products.map((product) => (
-          <li key={product.id}>
-            <Link to={`/product/${product.id}`}>
-              <img src={product.images[0].src} alt="Product banner" />
-              <h2>{product.name}</h2>
-              <p>Sale price: {product.sale_price}</p>
-              <strong>
-                {product.stock_status === "instock"
-                  ? "In stock"
-                  : "Out of stock"}
-              </strong>
-              <button>Add to Cart</button>
-            </Link>
-          </li>
-        ))
-      ) : (
-        <li>No products found</li>
-      )}
-    </ul>
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
   );
 }
 
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <>
+      <h1>Ojojoj, nu blidde're fel!</h1>
+    </>
+  );
+};
