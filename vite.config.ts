504c450839c90e99db4cdf337c1e956437f91f9a
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-17 16:50:08 +0200

    🚀 Base path config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     base: mode === "production" ? "/matpumpa/" : "/", // Set base for production
     define: {
-      "meta.env.BASE_PATH": JSON.stringify(
+      "process.env.BASE_PATH": JSON.stringify(
         mode === "production" ? "/matpumpa/" : "/"
       ),
     },
