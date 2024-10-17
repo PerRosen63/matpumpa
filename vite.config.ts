d437583dab87fff3672268fbe871903a57bb55ee
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-17 14:40:29 +0200

    🚀 Dynamic base path in router

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/matpumpa/" : "/",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/matpumpa/" : "/", // Set base for production
+    define: {
+      "process.env.BASE_PATH": JSON.stringify(
+        mode === "production" ? "/matpumpa/" : "/"
+      ),
+    },
+  };
 });
