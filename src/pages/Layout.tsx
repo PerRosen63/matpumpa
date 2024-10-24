1e3a7a9e3f94ec797b8909a586b0c71ea3ef0da4
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-24 20:50:43 +0200

    ✨ New pages

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,22 +3,32 @@ import { NavLink, Outlet } from "react-router-dom";
 export const Layout = () => {
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to={"/"}>Hem</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/products"}>Pumpor</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Outlet></Outlet>
-      </main>
-      <footer></footer>
+      <div className="text-yellow-custom">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to={"/"}>Hem</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/about"}>Om pumpor</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/growing"}>Odling & recept</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/products"}>Köp här</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <div className="border max-w-screen-xl mx-auto">
+            <Outlet></Outlet>
+          </div>
+        </main>
+        <footer></footer>
+      </div>
     </>
   );
 };
