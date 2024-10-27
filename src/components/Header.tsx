5f08e3e95c84d5d6aef5538ccea92ec9d844646b
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-28 00:42:59 +0100

    ✨ nav toggle mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <header>
-        <div className="border flex flex-row justify-between items-center max-w-screen-xl mx-auto p-5">
-          <div>
+        <div className="border relative flex flex-row justify-between items-center max-w-screen-xl mx-auto p-5">
+          <div className="max-w-48">
             <img
               width="250"
               height="auto"
@@ -14,19 +21,84 @@ export const Header = () => {
             />
           </div>
           <div>
-            <nav className="">
-              <ul className=" flex flex-row [&>li>a]:px-4 font-sans small-caps text-2xl text-yellow-custom-link hover:[&>li>a]:text-yellow-custom hover:[&>li>:not(a.active)]:bg-black/20 [&>li>a.active]:text-yellow-custom [&>li>a.active]:bg-black/20">
-                <li>
-                  <NavLink to={"/"}>Hem</NavLink>
+            <button
+              onClick={toggleMenu}
+              className="absolute right-3 top-3 z-10 md:hidden text-yellow-custom hover:text-white focus:text-white focus:outline-none"
+            >
+              <svg className="h-11 w-11 fill-current" viewBox="0 0 24 24">
+                {isOpen ? (
+                  <path
+                    fillRule="evenodd"
+                    d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                  />
+                ) : (
+                  <path
+                    fillRule="evenodd"
+                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+
+          <div>
+            <nav
+              className={`max-md:absolute left-0 top-0 bg-green-custom max-md:w-full max-md:border-b-2 border-gray-700 ${
+                isOpen ? "block" : "max-md:hidden max-md:sr-only"
+              }`}
+            >
+              <ul className="flex flex-col md:flex-row font-sans small-caps text-clamp-h6 text-yellow-custom-link">
+                <li className="py-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      [
+                        "block h-11 leading-[2.75rem] px-4 hover:text-yellow-custom hover:bg-black/20",
+                        isActive ? "bg-black/20" : "bg-transparent",
+                      ].join(" ")
+                    }
+                    to={"/"}
+                  >
+                    Hem
+                  </NavLink>
                 </li>
-                <li>
-                  <NavLink to={"/about"}>Om pumpor</NavLink>
+                <li className="py-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      [
+                        "block h-11 leading-[2.75rem] px-4 hover:text-yellow-custom hover:bg-black/20",
+                        isActive ? "bg-black/20" : "bg-transparent",
+                      ].join(" ")
+                    }
+                    to={"/about"}
+                  >
+                    Om pumpor
+                  </NavLink>
                 </li>
-                <li>
-                  <NavLink to={"/growing"}>Odling & recept</NavLink>
+                <li className="py-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      [
+                        "block h-11 leading-[2.75rem] px-4 hover:text-yellow-custom hover:bg-black/20",
+                        isActive ? "bg-black/20" : "bg-transparent",
+                      ].join(" ")
+                    }
+                    to={"/growing"}
+                  >
+                    Odling & recept
+                  </NavLink>
                 </li>
-                <li>
-                  <NavLink to={"/products"}>Köp här</NavLink>
+                <li className="py-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      [
+                        "block h-11 leading-[2.75rem] px-4 hover:text-yellow-custom hover:bg-black/20",
+                        isActive ? "bg-black/20" : "bg-transparent",
+                      ].join(" ")
+                    }
+                    to={"/products"}
+                  >
+                    Köp här
+                  </NavLink>
                 </li>
               </ul>
             </nav>
