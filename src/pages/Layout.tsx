e5c19a0d407df704b6272e5a69a9d06d3ffec089
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-11-25 16:55:58 +0100

    ⚡️ To top on navigate to route

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { useEffect } from "react";
 
 export const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top
+  }, [location]); // Run on route change
+
   return (
     <>
       <div className="flex flex-col text-yellow-custom min-h-screen">
