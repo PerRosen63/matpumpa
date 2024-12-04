71f6af4148301640ff16be42e0e3a5c78b3aec9f
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-12-04 21:45:58 +0100

    🚀 configs

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,8 @@ interface AppConfig {
 }
 
 const config: AppConfig = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
+  apiBaseUrl:
+    import.meta.env.VITE_API_BASE_URL || "https://mfdm.se/woo/wp-json/wc/v3",
   baseUrl: import.meta.env.VITE_BASE_URL,
   consumerKey: import.meta.env.VITE_CONSUMER_KEY,
   consumerSecret: import.meta.env.VITE_CONSUMER_SECRET,
